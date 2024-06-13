9c41dd32da0fe6cff706eec162f4c5b8aa569202
Rashidaga18 <rashidaga18@gmail.com>
2024-06-13 09:17:17 +0530

    Add some css in style.css and add Api in scipt .js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const dropList = document.querySelectorAll(".drop-list select");
+const dropList = document.querySelectorAll(".drop-list select"),
+ fromCurrency = document.querySelector(".from select"),
+ toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
 
 for( let i = 0 ; i < dropList.length; i++) {
     for(currency_code in country_code){
@@ -14,4 +17,24 @@ for( let i = 0 ; i < dropList.length; i++) {
         //inserting options tag inside select tag
         dropList[i].insertAdjacentHTML("beforeend" , optionTag);
     }
+}
+getButton.addEventListener("click",e =>{
+    e.preventDefault(); //preventing form from sumbitting
+    getExchangeRate();
+});
+
+function getExchangeRate(){
+    const amount = document.querySelector(".amount input");
+    let amountVal = amount.value;
+    //if user don't enter any value or enter 0 then we'll put 1 value by default in the input field
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    let url =`: https://v6.exchangerate-api.com/v6/c119e034a940b4457dff7ea9/latest/${fromCurrency.value}`;
+    //fetching api response and returning it with parsing into js obj and in another then method receiving that obj
+    fetch(url).then(response => response.json()).then (result =>{
+        let exchangeRate = result.conversion_rates
+        console.log(result);
+    })
 }
